5eaec1a150fc48d2857131dad1fe95c1a9d4ca56
vcing <2131287@qq.com>
2017-07-26 18:48:17 +0800

    add ielts answer

---
+++ ielts.js
@@ -0,0 +1,13 @@
+export const listening = {
+    'What work do you do?': `I work. I have been working in a game company for three years as a technical director or in the other words Chief Programmer.I love coding because I think it's the most creative work nowadays. And also it has a promising future.`,
+
+    'Do you miss being a student?': `No, I'm totally not. I don't like to be a student since I was about 14.I have my own goal, but I can't go for it when I was a student. It was tough days. After my college life, I finally have a chance to chase my goal. Now, everything or knowledge I used in my work is studied by my self. So, I really don't like to be a student at school`,
+
+    'Is it very interesting?': `Yes, of course. Thousands of people visit our website and play our game every day. I get a lot of comments from our lovely player who trying to encourage us to keep create good game. I think it's my pleasure that my work could bring them happiness`,
+
+    'Do you prefer to study in the mornings or in the afternoons': `I prefer to study in the morning.Because in the morning, I just wake up and I have very a clear mind. Also in the morning, it is very quiet, I can focus on my study. So I think it's the best time for me to study efficiently`,
+
+    'Do you like your subject': `Yes, I like it. Create a game or website which could bring thousands of people happiness give me a sense of achievement. I love that feeling. It's very hot-blooded to see my player left comments like "I like your game."`,
+
+    'Who teach you to be polite': `Well, like most people. My parents told me to be polite when I was a child. They told me to say "thank you" when I get help from other people.And when somebody gives me something, I should take it with my both hand to show my respect and so on. I think it is one of the great Chinese traditional cultures.`
+}
