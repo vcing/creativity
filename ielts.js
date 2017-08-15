d740b1bdb73819ea81f8b622aeb9a5a883c91213
vcing <2131287@qq.com>
2017-08-15 10:11:41 +0800

    update

--- ielts.js
+++ ielts.js
@@ -9,7 +9,31 @@ export const listening = {
 
     'Do you like your subject': `Yes, I like it. Create a game or website which could bring thousands of people happiness give me a sense of achievement. I love that feeling. It's very hot-blooded to see my player left comments like "I like your game."`,
 
-    'Who teach you to be polite': `Well, like most people. My parents told me to be polite when I was a child. They told me to say "thank you" when I get help from other people.And when somebody gives me something, I should take it with my both hand to show my respect and so on. I think it is one of the great Chinese traditional cultures.`
+    'Who teach you to be polite': `Well, like most people. My parents told me to be polite when I was a child. They told me to say "thank you" when I get help from other people.And when somebody gives me something, I should take it with my both hand to show my respect and so on. I think it is one of the great Chinese traditional cultures.`,
+
+    'Who is your favorite celebrity in China?': `My favorite celebrity named Zhang Shao Zhong. He is a navy general who dedicate to spread patriotic education on the internet. Not like other experts he is very kind, and like to share knowledge with youngers which are not common in China.Therefore he really changes my image to experts especially military experts.`,
+
+    'Would you want to be a celebrity in the future?': `Well. If possible why not. I like to be the center of attention, and I always long for the extravagant lifestyle that celebrities have, So I should definitely try that when I'm given a chance.`,
+
+    'In general, how do people become famous?': `Well. normally I think people become famous by attend some TV problem or TV competitions They either have great talent in doing something or their appearance is very impressive. So that would make them famous.`,
+
+    'What kinds of people become famous?': `People who have great talent in doing something or their appearance is ver impressive could make them famous. Such as my favorite celebrity Andy Lau. He is not only have great talent in acting or singing but also have a very impressive appearance.`,
+
+    'What kind of fruit do you like? why?': `I like coconut. Because it is really pure natural juice. Unlike most of other juice, coconuts needn't any other process. You can drink it directly.`,
+
+    'Did you like to eat fruit when you were a child?': `Yes. I did. I like fruit especially watermelon. It is really juicy and sweet. I can eat an 8-kilogram watermelon by myself when I was a child. In my memory, it is the taste of summer.`,
+
+    'Is it important to eat fruit?': `Definitely yes. Fruit, like vegetables, are nutritious and therefore are good for our health. Most fruits contain vitamins which human need that to boost our immune system. So I think eating fruit is necessary. `,
+
+    'How often do you buy shoes?': `I buy shoes about once a year. Because I just need one or two pairs of shoes in my daily life. The only reason I need to buy new shoes is that the older one is broken.`,
+
+    'Have you ever bought shoes online?': `Yes of course I have. Shopping online is a quick and easy way to buy what I want. On some special days, online store always has a lot of discount. Such as last double 11 shopping festival I bought two pairs of shoes. All of them have beautiful appearance and price.`,
+
+    'Do you know anyone who likes to buy a lot of shoes?': `Yes, my girlfriend is a really shoes collector. She buys shoes almost every week. But about half of her shoes, she just wore once or twice.`,
+
+    'What\'s is your favorite type of shoes?': `Well, my favorite type of shoes is the sandal. In hot summer only sandal could make my feet feel cool. And not like other shoes sandal is really easy to putting on or putting off. It's really convenient.`,
+
+    'How often do you wear a watch?': `I never wear a watch. Since I'm a programmer watch may hinder my coding. And if I need to know the time. I can check my mobile phone. So I don't find any reason to wear a watch.`
 }
 
 export const writing = {
